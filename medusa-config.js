b8fed94f8899d2cc93e22e31387789ab4459134f
Agustin Jalil Leon <agustinjalilleon@gmail.com>
2026-01-09 18:16:52 -0300

    gauth instalado

--- medusa-config.js
+++ medusa-config.js
@@ -162,11 +162,12 @@ const medusaConfig = {
         providers: [
 
           {
-            resolve: "@medusajs/auth/providers/google",
+            resolve: "@medusajs/medusa/auth-google",
+            id: "google",
             options: {
               clientId: process.env.GOOGLE_CLIENT_ID,
               clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-              callbackUrl: "/auth/customer/google/callback",
+              callbackUrl: process.env.GOOGLE_CALLBACK_URL,
             },
           },
 
